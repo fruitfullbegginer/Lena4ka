46c1e8d24712b326662030fe3c3a9fd996abfe30
Adamov Artem <lla83016@gmail.com>
2023-12-23 01:14:12 +0300

    Первая версия

---
+++ Zada4ka_Raz/Zada4ka_Raz.cpp
@@ -0,0 +1,10 @@
+// Zada4ka_Raz.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, ITMO!\n";
+}
+
